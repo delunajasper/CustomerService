f676eb9e7600f11d39a7f93786b0dfd8ffcd4910
jasperdeluna <delunajasper11@gmail.com>
2023-06-03 11:11:54 +0100

    Add Customer with DI from Service layer to Controller

---
+++ src/main/java/com/example/SpringBoot/customer/CustomerIRepo.java
@@ -0,0 +1,7 @@
+package com.example.SpringBoot.customer;
+
+import java.util.List;
+
+public interface CustomerIRepo {
+    List<Customer> getCustomers();
+}
---
+++ src/main/java/com/example/SpringBoot/customer/CustomerRepo.java
@@ -0,0 +1,4 @@
+package com.example.SpringBoot.customer;
+
+public class CustomerRepo {
+}
